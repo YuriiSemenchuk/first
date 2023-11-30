06d5c8256b169d874d1f629883a6e768c2aa7c12
YuriiSemenchuk <endercomyniti@gmail.com>
2023-11-30 16:43:58 +0200

    day16

--- day16/coffee_machine_oop.py
+++ day16/coffee_machine_oop.py
@@ -1,5 +1,20 @@
-from menu import Menu, MenuItem
+from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
 menu = Menu()
+continues = True
+
+while continues:
+    name_coffee = input(f"What would you like? ({menu.get_items()}): ")
+    if name_coffee == "off":
+        continues = False
+    elif name_coffee == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(name_coffee)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
