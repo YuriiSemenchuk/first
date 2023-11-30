0bfbbfefa3d9f7345e5bbf90cf62f084c82542ac
YuriiSemenchuk <endercomyniti@gmail.com>
2023-11-30 15:06:53 +0200

    problem

---
+++ day16/exercises.py
@@ -0,0 +1,26 @@
+def tables():
+    from prettytable import PrettyTable
+    table = PrettyTable()
+    table.add_column("Pokemon name", ["Pikachu", "Squirtle", "Charmander"])
+    table.add_column("Type", ["Electric", "Water", "Fire"])
+    table.align = "l"
+
+    print(table)
+
+
+def turtle():
+    from turtle import Turtle, Screen
+
+    timmy = Turtle()
+    print(timmy)
+    timmy.shape("turtle")
+    timmy.color("coral")
+
+    my_screen = Screen()
+    print(my_screen.canvheight)
+    timmy.forward(100)
+
+    my_screen.exitonclick()
+
+
+tables()
